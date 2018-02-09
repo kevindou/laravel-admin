4d1da9b45f0cefb4d889f8a377111cdd198b3445
liujinxing <jinxing.liu@verystar.cn>
2018-02-09 14:26:24 +0800

    修改Helper 助手类 添加一些方法

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -11,6 +11,65 @@ use Closure;
 class Helper
 {
     /**
+     * @var int json默认配置 JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE
+     */
+    const JSON_OPTIONS = JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE;
+
+    /**
+     * json encode 处理中文处理
+     *
+     * @param  mixed $mixed  转义数据
+     * @param  int $options 转义配置
+     *
+     * @return string 返回json字符串
+     */
+    public static function encode($mixed, $options = self::JSON_OPTIONS)
+    {
+        return json_encode($mixed, $options);
+    }
+
+    /**
+     * json decode 解析json字符串
+     *
+     * @param  string $json json字符串
+     * @param  bool $assoc  默认转成数组
+     *
+     * @return bool|array   数组或者false
+     */
+    public static function decode($json, $assoc = true)
+    {
+        return json_decode($json, $assoc);
+    }
+
+    /**
+     * 获取IP地址
+     *
+     * @return string 返回字符串
+     */
+    public static function getIpAddress()
+    {
+        if (isset($_SERVER)) {
+            if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+                $strIpAddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            } else if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+                $strIpAddress = $_SERVER['HTTP_CLIENT_IP'];
+            } else {
+                $strIpAddress = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '';
+            }
+        } else {
+            if (getenv('HTTP_X_FORWARDED_FOR')) {
+                $strIpAddress = getenv('HTTP_X_FORWARDED_FOR');
+            } else if (getenv('HTTP_CLIENT_IP')) {
+                $strIpAddress = getenv('HTTP_CLIENT_IP');
+            } else {
+                $strIpAddress = getenv('REMOTE_ADDR') ? getenv('REMOTE_ADDR') : '';
+            }
+        }
+
+        return $strIpAddress;
+    }
+
+    /**
      * 处理查询参数信息
      *
      * @param \Illuminate\Database\Query\Builder $query 查询对象
@@ -32,22 +91,23 @@ class Helper
 
             if ($value instanceof Closure) {
                 $value($query, $column, $params[$column]);
-            } else {
-                if (is_array($value)) {
-                    $column = isset($value['column']) ? $value['column'] : $column;
-                    $operator = isset($value['operator']) ? $value['operator'] : '=';
-                    if (
-                        isset($value['callback']) &&
-                        (function_exists($value['callback']) || $value['callback'] instanceof Closure)
-                    ) {
-                        $params[$column] = $value['callback']($params[$column]);
-                    }
-                } else {
-                    $operator = (string)$value;
-                }
-
-                self::handleMethod($query, $column, $operator, $params[$column]);
+                continue;
             }
+
+            if (is_array($value)) {
+                $column = isset($value['column']) ?: $column;
+                $operator = isset($value['operator']) ?: '=';
+                if (
+                    isset($value['callback']) &&
+                    (function_exists($value['callback']) || $value['callback'] instanceof Closure)
+                ) {
+                    $params[$column] = $value['callback']($params[$column]);
+                }
+            } else {
+                $operator = (string) $value;
+            }
+
+            static::handleMethod($query, $column, $operator, $params[$column]);
         }
     }
 
@@ -67,14 +127,17 @@ class Helper
             case 'between':
             case 'not between':
                 $methods = explode(' ', $operator);
-                foreach ($methods as &$val) $val = ucfirst($val);
+                foreach ($methods as &$val) {
+                    $val = ucfirst($val);
+                }
+
                 unset($val);
                 $strMethod = 'where' . implode('', $methods);
                 $query->{$strMethod}($column, $value);
                 break;
             case 'like':
             case 'not like':
-                $query->where($column, $operator, '%' . (string)$value . '%');
+                $query->where($column, $operator, '%' . (string) $value . '%');
                 break;
             default:
                 $query->where($column, $operator, $value);
