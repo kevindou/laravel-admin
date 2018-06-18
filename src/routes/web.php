07bc5cff97a392b911ac5d3fe175c53a792270a1
jinxing.liu <821901008@qq.com>
2018-06-18 22:16:09 +0800

    update laravel-admin

---
+++ src/routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+/* @var \Illuminate\Support\Facades\Route */
+use \Illuminate\Support\Facades\Route;
+
+$uri    = trim(request()->getPathInfo(), '/');
+$params = isset($uri[0]) ? explode('/', $uri) : [];
+foreach ($params as $i => $v) {
+    $vv         = explode('-', $v);
+    $params[$i] = implode('', array_map(function ($v) {
+            return ucfirst($v);
+        }, $vv)
+    );
+}
+
+// 去掉admin 控制器
+array_shift($params);
+
+$defaultController = 'IndexController';
+$defaultAction     = 'actionIndex';
+switch (count($params)) {
+    case 0:
+        $controller = $defaultController;
+        $action     = $defaultAction;
+        break;
+    case 1:
+        $controller = $params[0] . 'Controller';
+        $action     = $defaultAction;
+        break;
+    default:
+        $action     = 'action' . ucfirst($params ? array_pop($params) : 'index');
+        $controller = ($params ? implode('\\', array_map('ucfirst', $params)) : 'Index') . 'Controller';
+        break;
+}
+
+$namespace  = '\App\Http\Controllers\Admin\\';
+$options    = ['domain' => request()->getHttpHost(), 'namespace' => $namespace];
+$controller = $namespace . $controller;
+
+if (method_exists($controller, $action)) {
+    Route::group($options, function () use ($uri, $controller, $action) {
+        Route::match(['get', 'post'], $uri, $controller . '@' . $action);
+    });
+}
+
+
+
