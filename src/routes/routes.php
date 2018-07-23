21976f7a7cd9dc5a1056a98b3d5aad35bf772f61
liujinxing <jinxing.liu@verystar.cn>
2018-07-23 17:45:56 +0800

    添加注释

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -2,26 +2,22 @@
 
 use \Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'admin'], function () {
-    $uri    = request()->path();
-    $params = isset($uri[0]) ? explode('/', $uri) : [];
-    // 因为是admin 前缀分组信息，那么下面的路由需要去掉 admin 前缀
-    array_shift($params);
-    $uri = implode('/', $params);
-    // 将 get-user-info 转为 GetUserInfo
-    array_studly_case($params);
-    // 获取到 控制器 和 action
-    list($controller, $action) = get_controller_action($params);
+$uri    = trim(request()->getPathInfo(), '/');
+$params = $uri == '' ? [] : explode('/', $uri);
+// 将 get-user-info 转为 GetUserInfo
+array_studly_case($params);
+// 获取到 控制器 和 action
+list($controller, $action) = get_controller_action($params);
 
-    $namespace  = '\App\Http\Controllers\Admin\\';
-    $options    = ['domain' => request()->getHttpHost(), 'namespace' => $namespace];
-    $controller = $namespace . $controller;
-    if (method_exists($controller, $action)) {
-        Route::group($options, function () use ($uri, $controller, $action) {
-            Route::match(['get', 'post'], $uri, $controller . '@' . $action);
-        });
-    }
-});
+$namespace  = '\App\Http\Controllers\\';
+$options    = ['domain' => request()->getHttpHost(), 'namespace' => $namespace];
+$controller = $namespace . $controller;
+if (method_exists($controller, $action)) {
+    Route::group($options, function () use ($uri, $controller, $action) {
+        Route::match(['get', 'post'], $uri, $controller . '@' . $action);
+    });
+}
+
 
 
 
